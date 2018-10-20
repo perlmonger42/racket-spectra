6b4c75560a4e4d48690dd924b15a5e48546bd197
Thom Boyer <thom@boyers.org>
2018-10-20 10:15:28 -0600

    Use lexer-srcloc instead of lexer

--- tokenizer.rkt
+++ tokenizer.rkt
@@ -9,59 +9,55 @@
   (require rackunit))
 
 (define (spectra-token? x)
-  (or (eof-object? x) (string? x) (token-struct? x)))
+  (or (eof-object? x) (srcloc-token? x)))
 
 (module+ test
   (check-true (spectra-token? eof))
-  (check-true (spectra-token? "a string"))
-  (check-true (spectra-token? (token 'A-TOKEN-STRUCT "hi")))
-  (check-false (spectra-token? 42)) )
+  (check-true (spectra-token? (srcloc-token 'hi (srcloc #f 1 0 1 2)))))
 
 (define (make-tokenizer port)
   (port-count-lines! port)
   (define (next-token)
     (define spectra-lexer
-      (lexer
-       [(from/to "//" "\n") (next-token)]
+      (lexer-srcloc
+       [(from/to "//" "\n") (token 'COMMENT lexeme #:skip? #t)]
        [(from/to "@$" "$@")
-        (token 'SEXP-TOK (trim-ends "@$" lexeme "$@")
-               #:position (+ (pos lexeme-start) 2)
-               #:line (line lexeme-start)
-               #:column (+ (col lexeme-start) 2)
-               #:span (- (pos lexeme-end) (pos lexeme-start) 4))]
-       [any-char (token 'CHAR-TOK lexeme
-                        #:position (pos lexeme-start)
-                        #:line (line lexeme-start)
-                        #:column (col lexeme-start)
-                        #:span (- (pos lexeme-end) (pos lexeme-start)))]))
+        (token 'SEXP-TOK (trim-ends "@$" lexeme "$@"))]
+       [any-char (token 'CHAR-TOK lexeme)]))
     (spectra-lexer port))
   next-token)
 
 (module+ test
   (check-equal?
    (apply-tokenizer-maker make-tokenizer "// comment\n")
-   empty)
+   (list (srcloc-token (token 'COMMENT "// comment\n" #:skip? #t)
+                       (srcloc 'string 1 0 1 11))))
+   ; The srcloc fields are:
+   ;   source : any/c
+   ;   line : (or/c exact-positive-integer? #f)            1-based
+   ;   column : (or/c exact-nonnegative-integer? #f)       0-based
+   ;   position : (or/c exact-positive-integer? #f)        1-based
+   ;   span : (or/c exact-nonnegative-integer? #f)         0-based
+   ; The `source` is 'string in all these tests because the lexer
+   ; input is a string (rather than a file, for example).
   (check-equal?
    (apply-tokenizer-maker make-tokenizer "@$ (+ 6 7) $@")
-   (list (token 'SEXP-TOK " (+ 6 7) "
-                #:position 3
-                #:line 1
-                #:column 2
-                #:span 9)))
+   (list (srcloc-token (token 'SEXP-TOK " (+ 6 7) ")
+                       (srcloc 'string 1 0 1 13))))
   (check-equal?
    (apply-tokenizer-maker make-tokenizer "hi")
-   (list (token 'CHAR-TOK "h"
-                #:position 1
-                #:line 1
-                #:column 0
-                #:span 1)
-         (token 'CHAR-TOK "i"
-                #:position 2
-                #:line 1
-                #:column 1
-                #:span 1)))
+   (list (srcloc-token (token 'CHAR-TOK "h")
+                       (srcloc 'string 1 0 1 1))
+         (srcloc-token (token 'CHAR-TOK "i")
+                       (srcloc 'string 1 1 2 1))))
+
   (check-equal?
    (apply-tokenizer-maker make-tokenizer "//x\ny\nz")
-   (list (token-struct 'CHAR-TOK "y" 5 2 0 1 #f)
-         (token-struct 'CHAR-TOK "\n" 6 2 1 1 #f)
-         (token-struct 'CHAR-TOK "z" 7 3 0 1 #f))))
+   (list (srcloc-token (token 'COMMENT "//x\n" #:skip? #t)
+                       (srcloc 'string 1 0 1 4))
+         (srcloc-token (token 'CHAR-TOK "y")
+                       (srcloc 'string 2 0 5 1))
+         (srcloc-token (token 'CHAR-TOK "\n")
+                       (srcloc 'string 2 1 6 1))
+         (srcloc-token (token 'CHAR-TOK "z")
+                       (srcloc 'string 3 0 7 1)))))
